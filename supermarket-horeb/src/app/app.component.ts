9357770579aac0f4627d32d7f9d06dbb638c7321
Duvansg06 <140823687+Duvansg06@users.noreply.github.com>
2026-04-22 16:24:19 -0500

    todo ok

--- supermarket-horeb/src/app/app.component.ts
+++ supermarket-horeb/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from './components/hero/hero.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeroComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
